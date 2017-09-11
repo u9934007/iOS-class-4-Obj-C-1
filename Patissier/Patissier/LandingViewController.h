d40a1684e09b045774ffa150751ee5cd67aa4185
brady800526 <brady800526@gmail.com>
2017-09-11 10:47:51 +0800

    new landing page

---
+++ Patissier/Patissier/LandingViewController.h
@@ -0,0 +1,13 @@
+//
+//  LandingViewController.h
+//  Patissier
+//
+//  Created by Brady Huang on 2017/9/11.
+//  Copyright © 2017年 Brady Huang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LandingViewController : UIViewController
+
+@end
---
+++ Patissier/Patissier/LandingViewController.m
@@ -0,0 +1,33 @@
+//
+//  LandingViewController.m
+//  Patissier
+//
+//  Created by Brady Huang on 2017/9/11.
+//  Copyright © 2017年 Brady Huang. All rights reserved.
+//
+
+#import "LandingViewController.h"
+
+@interface LandingViewController ()
+
+@end
+
+@implementation LandingViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
