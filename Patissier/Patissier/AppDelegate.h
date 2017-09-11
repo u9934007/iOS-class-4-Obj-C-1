0871e278e03111e052ccebd8eb84bc741adb13c7
brady800526 <brady800526@gmail.com>
2017-09-11 09:36:25 +0800

    start of the project

---
+++ Patissier/Patissier/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Patissier
+//
+//  Created by Brady Huang on 2017/9/11.
+//  Copyright © 2017年 Brady Huang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
